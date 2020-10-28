c18fbc0461059e5554263b7390395f1a42f0f370
Ibrahim Ahmad <ibrahim.ahmad@SAPNS2.com>
2020-10-28 17:25:33 -0400

    Basic Functionality and Skeleton

--- bot.py
+++ bot.py
@@ -1,19 +1,119 @@
-#Implementation of Bot connection to discord 
-
-import os
-
+import firebase_admin
+from firebase_admin import credentials, firestore
 import discord 
 from discord.ext import commands
+import datetime
+
+cred = credentials.Certificate("./botprivatekey.json")
+firebase_admin.initialize_app(cred)
+
+
+db = firestore.client()  
+
 
 client = commands.Bot(command_prefix = '!')
 
 @client.event
 async def on_ready():
-        print("Bot Is Ready")
+    print("Bot Is Ready")
+        
+
+event_id = ''
+event_in_progress_flag = False
 
 @client.command()
-async def hello(ctx):
-        await ctx.send('Hello!') 
+async def start(ctx):
+
+    global event_in_progress_flag
+    global event_id
+
+    if event_in_progress_flag:
+        await ctx.send('Event already in progress, use the join command')
+        return
+
+    collection = db.collection('events')
+    event_id = datetime.datetime.now()
+    res = collection.document(f'{event_id}').set({'id': f'{event_id}'})
+    
+    collection = db.collection('event_participants')
+    res = collection.document(f'{event_id}').set({'event_id': f'{event_id}'})
+    res = collection.document(f'{event_id}').update({'participants': {}})
+
+    await ctx.send('Event has started')
+    await ctx.send('If you would like to participate, use the join command')
+    event_in_progress_flag = True
+
+@client.command()
+async def join(ctx):
+
+    global event_id
+    global event_in_progress_flag
+
+    if not event_in_progress_flag:
+        await ctx.send('No event in progress, use the start command to begin an event')
+        return
+
+    msg_auth = ctx.message.author.id
+
+    user_dicts = [ item.to_dict() for item in db.collection('users').get() ]
+    for user in user_dicts:
+        if msg_auth in user.values():
+            break
+        else:
+            collection = db.collection('users')
+            res = collection.document(f'{msg_auth}').set({'id': msg_auth})
+
+    event_participants_dicts = [ item.to_dict() for item in db.collection('event_participants').get() ]
+    for event_participant in event_participants_dicts:
+        if (str(event_id) == event_participant['event_id']) and (str(msg_auth) in event_participant['participants']):
+            await ctx.send(f'{ctx.message.author}, you have already been added to the event! :rage:')
+            return
+        else:
+            collection = db.collection('event_participants')
+            res = collection.document(f'{event_id}').update({'participants': {f'{msg_auth}': [] } } )
+
+    await ctx.send(f'{ctx.message.author} has been added to the event! :partying_face:')
+
+    user = client.get_user(msg_auth)
+    await user.send("Please use this channel to construct your wishlist using the add command")
+
+@client.command()
+async def add(ctx, *, wish):
+
+    global event_id
+    global event_in_progress_flag
+
+    if not event_in_progress_flag:
+        await ctx.send('No event in progress, use the start command to begin an event')
+        return
+
+    msg_auth = ctx.message.author.id
+
+    collection = db.collection('event_participants')
+    res = collection.document(f'{event_id}').update({'participants': {f'{msg_auth}': firestore.ArrayUnion([f'{wish}']) } } )
+
+    user = client.get_user(msg_auth)
+    await user.send(f'I added {wish} to your wishlist :upside_down:')
+
+@client.command(aliases=['list'])
+async def _list(ctx):
+    global event_id
+    global event_in_progress_flag
+
+    if not event_in_progress_flag:
+        await ctx.send('No event in progress, use the start command to begin an event')
+        return
+
+    msg_auth = ctx.message.author.id
+
+    event_participants_dict = db.collection('event_participants').document(f'{event_id}').get().to_dict()
+    print(event_participants_dict)
+    user = client.get_user(msg_auth)
+    ulist = event_participants_dict['participants'].get(f'{msg_auth}')
+    print(ulist)
+    await user.send(f'Here is your wishlist: {ulist}')
+    print('done')
+            
 
 
-client.run('')
+client.run('bot_token')

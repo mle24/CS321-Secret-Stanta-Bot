3939b1e6cbc327f833d5908c3e5ae1de31e44c2a
mle24 <matthewdle24@gmail.com>
2020-11-02 21:11:34 -0500

    Cleanup DB Architecture

--- bot.py
+++ bot.py
@@ -15,70 +15,69 @@ client = commands.Bot(command_prefix = '!')
 async def on_ready():
     print("Bot Is Ready")
         
-event_id = ''
-event_in_progress_flag = False
+
+
 
 @client.command()
 async def start(ctx):
+    #Starts Event
 
     global event_in_progress_flag
     global event_id
 
-    if event_in_progress_flag:
-        await ctx.send('Event already in progress, use the join command')
-        return
-
-    collection = db.collection('events')
-    event_id = datetime.datetime.now()
-    res = collection.document(f'{event_id}').set({'id': f'{event_id}'})
+   
     
-    collection = db.collection('event_participants')
-    res = collection.document(f'{event_id}').set({'event_id': f'{event_id}'})
-    res = collection.document(f'{event_id}').update({'participants': {}})
-
-    await ctx.send('Event has started')
-    await ctx.send('If you would like to participate, use the join command')
-    event_in_progress_flag = True
+    author_id = ctx.message.author.id
+    guild_id = ctx.message.guild.id
+    
+    doc_ref = db.collection('events').document(f'{guild_id}')
+    if doc_ref.get().exists: 
+       await ctx.send('Server already has an ongoing event')
+    else:
+        doc_ref.set({
+            'host': f'{author_id}',
+            'users': []
+        })
+        await ctx.send('Event has started')
+        await ctx.send('If you would like to participate, use the join command') 
+        
+ 
+    #event_in_progress_flag = True
 
 @client.command()
 async def join(ctx):
-
-    global event_id
-    global event_in_progress_flag
-
-    if not event_in_progress_flag:
+    '''
+        User Join Event if not already participating
+    '''
+    author_id = ctx.message.author.id
+    guild_id = ctx.message.guild.id
+    event_ref = db.collection('events').document(f'{guild_id}')
+    user_ref = db.collection('users').document(f'{author_id}')
+    
+    #check if event exist
+    if not event_ref.get().exists: 
         await ctx.send('No event in progress, use the start command to begin an event')
         return
 
-    msg_auth = ctx.message.author.id
-
-    user_dicts = [ item.to_dict() for item in db.collection('users').get() ]
-    for user in user_dicts:
-        if msg_auth in user.values():
-            break
-        else:
-            collection = db.collection('users')
-            res = collection.document(f'{msg_auth}').set({'id': msg_auth})
-
-    event_participants_dicts = [ item.to_dict() for item in db.collection('event_participants').get() ]
-    for event_participant in event_participants_dicts:
-        if (str(event_id) == event_participant['event_id']):
-            
-            if (str(msg_auth) in event_participant['participants']):
-                await ctx.send(f'{ctx.message.author}, you have already been added to the event! :rage:')
-                return
-            else:
-                collection = db.collection('event_participants')
-                res = collection.document(f'{event_id}').update({f'participants.{msg_auth}': []} )
+    
+    #check if user exist in users collection
+    if not user_ref.get().exists:
+        user_ref.set({
+            'wishlist': [], 
+        })
+    
+    #add user to list of users in the server
+    event_ref.update({'users': firestore.ArrayUnion([user_ref])})
 
     await ctx.send(f'{ctx.message.author} has been added to the event! :partying_face:')
+    
 
-    user = client.get_user(msg_auth)
-    await user.send("Please use this channel to construct your wishlist using the add command")
-
+#FIX-ME
 @client.command()
 async def add(ctx, *, wish):
-
+    '''
+        Add item into wishlist
+    '''
     global event_id
     global event_in_progress_flag
 
@@ -94,8 +93,13 @@ async def add(ctx, *, wish):
     user = client.get_user(msg_auth)
     await user.send(f'I added {wish} to your wishlist :upside_down:')
 
+
+#FIX-ME
 @client.command(aliases=['list'])
 async def _list(ctx):
+    '''
+        Retrieve recipeint wishlist 
+    '''
     global event_id
     global event_in_progress_flag
 
@@ -111,7 +115,21 @@ async def _list(ctx):
     ulist = event_participants_dict['participants'].get(f'{msg_auth}')
 
     await user.send(f'Here is your wishlist: {ulist}')
-            
+
+#Test          
+@client.command()
+async def randomize(ctx): 
+    user_dicts = [ item.to_dict() for item in db.collection('users').get() ]
+    
+    for user in user_dicts:
+        print(user['id'])
+        if user['id']  != 0: 
+            user = client.get_user(user['id'])
+            if user != None:
+                await user.send('Hello')
+            else: 
+                await ctx.send('failed:(')
+        print('--------------')
 
 
-client.run('NzY1MzgxOTA3MTM5MTMzNTIw.X4T_cg.mE8yghJf8AS8sjiPnDvG7xDPM1M')
+client.run('')

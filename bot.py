4867e0cbdbc94d35e605d90e88437c825b39d22f
Affan Ahmed <affanahmed@Affans-MBP.fios-router.home>
2020-11-09 16:37:27 -0500

    Permissions feature and other changes added

--- bot.py
+++ bot.py
@@ -1,171 +1,208 @@
 import firebase_admin
 from firebase_admin import credentials, firestore
-
-import discord  
+import discord
 from discord.ext import commands
-
 import datetime
 import random
 
+# Initialize credentials for bot to access database
 cred = credentials.Certificate("./botprivatekey.json")
 firebase_admin.initialize_app(cred)
-
-db = firestore.client() 
-
-client = commands.Bot(command_prefix = '!')
-
-@client.event
-async def on_ready():
-    print("Bot Is Ready")
-        
+# Initialize database client and discord bot
+db = firestore.client()
+bot = commands.Bot(command_prefix='!')
 
 
-
-@client.command()
+@bot.command()
 async def start(ctx):
     '''
-        Starts Event
+        The host starts the event
     '''
+    # Make sure this command is run on a server
+    if isinstance(ctx.channel, discord.channel.DMChannel):
+        await ctx.send("This command can only be run in a server! :smile:")
+        return
+    # Get references to the user and the server they are on
     author_id = ctx.message.author.id
     guild_id = ctx.message.guild.id
-    
-    doc_ref = db.collection('events').document(f'{guild_id}')
-    if doc_ref.get().exists: 
-       await ctx.send('Server already has an ongoing event')
-       await ctx.send('If you would like to participate, use the !join command') 
-    else:
-        doc_ref.set({
+    event_ref = db.collection('events').document(f'{guild_id}')
+    # Check if event exists in the events collection
+    if not event_ref.get().exists:
+        # If the event does not exist, create the event
+        event_ref.set({
             'host': f'{author_id}',
             'users': []
         })
-        await ctx.send('Event has started')
-        await ctx.send('If you would like to participate, use the join command') 
-        
- 
-    #event_in_progress_flag = True
+        await ctx.send('Event has started! :christmas_tree:')
+    else:
+        # Send message if the event is already started
+        await ctx.send('Server already has an ongoing event! :christmas_tree:')
+    # Send message on how the user can join the event
+    await ctx.send('If you would like to participate, use the !join command')
 
-@client.command()
+
+@bot.command()
 async def join(ctx):
     '''
-        User Join Event if not already participating
+        User joins the even if not already participating
     '''
+    # Make sure this command is run on a server
+    if isinstance(ctx.channel, discord.channel.DMChannel):
+        await ctx.send("This command can only be run in a server! :smile:")
+        return
+    # Get references to the server they are on
     author_id = ctx.message.author.id
     guild_id = ctx.message.guild.id
     event_ref = db.collection('events').document(f'{guild_id}')
     user_ref = db.collection('users').document(f'{author_id}')
-    
-    #check if event exist
-    if not event_ref.get().exists: 
-        await ctx.send('No event in progress, use the start command to begin an event')
+    # Make sure an event is in progress
+    if not event_ref.get().exists:
+        await ctx.send('No event in progress, ask the event host to start one! :smile:')
         return
-
-    user = client.get_user(author_id)
-    #check if user exist in users collection
+    # Get the user who is joining the event
+    user = bot.get_user(author_id)
+    # Check if user exists in our database of users
     if not user_ref.get().exists:
+        # If they don't exist, set their information in the database
         user_ref.set({
-            'name' : f'{user.name}',
-            'wishlist': [], 
+            'name': f'{user.name}#{user.discriminator}',
+            'wishlist': [],
+            'events': [],
         })
-    
-    #add user to list of users in the server
-    event_ref.update({'users': firestore.ArrayUnion([user_ref])})
+    # If the event we are joining already exists in the users event list, send them a message
+    if event_ref in user_ref.get().to_dict()['events']:
+        await ctx.send('You have already joined the event! :smile:')
+    # If the user is not part of the event we are joining, add it to their event list and
+    # add the user to the servers event list.
+    else:
+        event_ref.update({'users': firestore.ArrayUnion([user_ref])})
+        user_ref.update({'events': firestore.ArrayUnion([event_ref])})
+        await ctx.send(f'{ctx.message.author} has been added to the event! :partying_face:')
+        await user.send("Use this channel to add and remove items from your wishlist")
 
-   
-    await ctx.send(f'{ctx.message.author} has been added to the event! :partying_face:')
-    await user.send("Use this channel to add/remove items to your wishlist")
-    
-    
 
-
-@client.command()
-async def add(ctx, *, wish):
+@bot.command()
+async def add(ctx, *args):
     '''
         Add item into wishlist
     '''
-   
+    # Get references to the user
     author_id = ctx.message.author.id
-   
-   
     user_ref = db.collection('users').document(f'{author_id}')
-    
+    # Create item to be added into wishlist
+    item = " ".join(args)
+    # Make sure the user exists
+    if not user_ref.get().exists:
+        await ctx.send('You are not part of an event! Join one before adding items to your wishlist :smile:')
+        return
+    # User is not part of any event throughout Discord
+    if len(user_ref.get().to_dict()['events']) == 0:
+        await ctx.send('You are not part of an event! Join one before adding items to your wishlist :smile:')
+        return
+    # Alert user that their item can be seen in public
+    user = bot.get_user(author_id)
+    if not isinstance(ctx.channel, discord.channel.DMChannel):
+        await user.send("Others can see the items you add to your wishlist in a public channel! Be careful! :smile:")
+    # Update user refernece with the new array of items
+    user_ref.update({'wishlist': firestore.ArrayUnion([f'{item}'])})
+    user = bot.get_user(author_id)
+    await user.send(f"I have added {item} to your wishlist :upside_down:")
 
-    user_ref.update({'wishlist': firestore.ArrayUnion([f'{wish}'])})
-    user = client.get_user(author_id) 
-    await user.send(f'I added {wish} to your wishlist :upside_down:')
 
-
-#FIX-ME  
-@client.command(aliases=['list'])
-async def _list(ctx):
+@bot.command()
+async def remove(ctx, *args):
     '''
-        Retrieve recipeint wishlist 
+        Remove item from wishlist
     '''
-
+    # Get references to the user
     author_id = ctx.message.author.id
-    guild_id = ctx.message.guild.id
-    event_ref = db.collection('events').document(f'{guild_id}')
-    event = event_ref.get().to_dict()
-    
-    rec_ref = event[f'{author_id}']
-    wishlist = rec_ref.get().to_dict()['wishlist']
+    user_ref = db.collection('users').document(f'{author_id}')
+    # Create item to be added into wishlist
+    item = " ".join(args)
+    # Make sure the user exists
+    if not user_ref.get().exists:
+        await ctx.send('You are not part of an event! Join one before removing items from your wishlist :smile:')
+        return
+    # User is not part of any event throughout Discord
+    if len(user_ref.get().to_dict()['events']) == 0:
+        await ctx.send('You are not part of an event! Join one before removing items from your wishlist :smile:')
+        return
+    # Alert user that their item can be seen in public
+    user = bot.get_user(author_id)
+    if not isinstance(ctx.channel, discord.channel.DMChannel):
+        await user.send("Others can see the items you remove from your wishlist in a public channel! Be careful! :smile:")
+    # Update user refernece and delete specified item
+    user_ref.update({'wishlist': firestore.ArrayRemove([f'{item}'])})
+    user = bot.get_user(author_id)
+    await user.send(f"I have deleted {item} from your wishlist :upside_down:")
 
-    user = client.get_user(int(author_id))
-    
-    await user.send(f'Here is the wishlist: {wishlist}')
-    
 
- 
-
-#Fix-Me     
-@client.command()
-async def randomize(ctx): 
+@bot.command()
+async def wishlist(ctx):
     '''
-        Randomly pair users
+        Retrieve your wishlist
     '''
+    # Get references to the user
+    author_id = ctx.message.author.id
+    user_ref = db.collection('users').document(f'{author_id}')
+    # Make sure the user exists
+    if not user_ref.get().exists:
+        await ctx.send('You are not part of an event! Join one before viewing items in your wishlist :smile:')
+        return
+    # User is not part of any event throughout Discord
+    if len(user_ref.get().to_dict()['events']) == 0:
+        await ctx.send('You are not part of an event! Join one before viewing items in your wishlist :smile:')
+        return
+    user = bot.get_user(int(author_id))
+    wishlist = user_ref.get().to_dict()['wishlist']
+    await user.send(f'Here is your wishlist: {wishlist}')
 
+
+@bot.command()
+async def pair(ctx):
+    '''
+        Randomly pair users with each other
+    '''
+    # Make sure this command is run on a server
+    if isinstance(ctx.channel, discord.channel.DMChannel):
+        await ctx.send("This command can only be run in a server! :smile:")
+        return
+    # Create empty dictionary for pairs
     pairs = {}
-    guild_id = ctx.message.guild.id
+    # Get references to the user and the server they are on
     author_id = ctx.message.author.id
-
-   
+    guild_id = ctx.message.guild.id
     event_ref = db.collection('events').document(f'{guild_id}')
+    # Make sure an event is in progress
+    if not event_ref.get().exists:
+        await ctx.send('No event in progress, ask the event host to start one! :smile:')
+        return
     event = event_ref.get().to_dict()
     users = event['users']
-    host = event['host']
-
-    #host permission
-    if author_id != int(host): 
-        await ctx.send('Only the host can start random pairings :(')
+    # Make sure host is the only one calling this command
+    if author_id != int(event['host']):
+        host = bot.get_user(int(event['host']))
+        await ctx.send(f'Only the event host can run this command. Ask {host.name}#{host.discriminator}! :smile:')
         return
-
-    #randomize list of users
+    # Make sure there are users present in the event
+    if len(users) == 0:
+        await ctx.send('No users have joined the event! Ask them to join with the !join command :smile:')
+        return
+    # Randomize list of users
     random.shuffle(users)
+    # Pairs users via Hamiltonian Cycle
+    for i in range(len(users)):
+        pairs[users[i].id] = users[(i+1) % len(users)]
+    # Update database
+    event_ref.update({"pairs": pairs})
+    # Loop through all the pairs and send them a message with who their pair is
+    for key, value in pairs.items():
+        user = bot.get_user(int(key))
+        recipient = value.get().to_dict()['name']
+        recipient_wishlist = value.get().to_dict()['wishlist']
+        await user.send(f'Your recipient is {recipient}!!! :santa:\nHere is their wishlist: {recipient_wishlist} :smile:')
+    # Send a message to the guild stating that everyone has been paired
+    await ctx.send('Everyone who joined the event has been randomly paired! Check your direct messages to see who you are paired with. :smile:')
 
-    #pairs users via Hamiltonian Cycle
-    for i in range(len(users)): 
-        pairs[users[i].id] = users[(i+1)%len(users)]
 
-
-    event_ref.update(pairs)
-    await ctx.send('Everyone has been randomly matched run !recipient in the server to get your recipient:)')
-
-
-@client.command()
-async def recipient(ctx): 
-    '''
-        get recipient
-    '''
-    guild_id = ctx.message.guild.id
-    author_id = ctx.message.author.id
-
-    event_ref = db.collection('events').document(f'{guild_id}')
-    event = event_ref.get().to_dict()
-
-    rec_ref = event[f'{author_id}']
-    rec_name = rec_ref.get().to_dict()['name']
-
-    user = client.get_user(author_id)
-
-    await user.send(f'Your recipient is {rec_name}!!! :santa: ')
-
-client.run('')
+bot.run('')
